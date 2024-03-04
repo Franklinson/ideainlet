e82b6a805537fac8eb19f3570b005c69dd4090a4
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2024-03-04 12:11:40 +0300

    Minor edits to signal.py

--- abstract/signals.py
+++ abstract/signals.py
@@ -5,6 +5,15 @@ from django.contrib.auth.models import Group
 from .models import Author
 
 def author_profile(sender, instance, created, **kwargs):
+	"""
+    Creates an author profile whenever a new user is saved.
+
+    Args:
+        sender (type): The model class that sent the signal.
+        instance (User): The specific User instance that was saved.
+        created (bool): A boolean indicating whether the User instance was created or updated.
+        **kwargs: Additional keyword arguments passed to the signal.
+    """
 	if created:
 		group = Group.objects.get(name='author')
 		instance.groups.add(group)
